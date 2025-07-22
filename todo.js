07cb979de7b9f3b413db1ffd3fe3d34dd7253c4f
HemasaiAmujuri <hemasaiamujuri3@gmail.com>
2025-07-22 04:33:17 -0700

    working on todo

--- todo.js
+++ todo.js
@@ -1,15 +1,49 @@
 function addTask() {
-  const tasklist = document.getElementById("task-list");
-  const value = document.getElementById("userInput").value;
-  let listItem = document.createElement("li");
-  listItem.innerText = value;
-  tasklist.appendChild(listItem);
-  const edit = document.createElement("button");
-  edit.textContent = "Edit"
-  edit.classList.add('medium-buttons')
-  tasklist.appendChild(edit);
+  const tasks = document.getElementById("tasks");
+  const input = document.getElementById("userInput");
+  const userTask = input.value;
+  if (userTask == "") {
+    alert("please enter your task");
+    return;
+  }
+  let container = document.createElement("div");
+  container.className = "nested-div";
+  let paragraph = document.createElement("p");
+  paragraph.className = "target-paragraph";
+  paragraph.textContent = userTask;
+  container.appendChild(paragraph);
+  input.value = "";
+  
+  const editbtn = document.createElement("button");
+  editbtn.textContent = "Edit";
+  editbtn.className="medium-buttons";
+  editbtn.onclick = () => {
+    const tasks = paragraph.textContent;
+    console.log(tasks)
+    const input1 = document.createElement("input");
+    input1.type = "text";
+    input1.value = tasks;
+    console.log(input1);
+    container.insertBefore(input1,paragraph);
+  };
+  
+  container.appendChild(editbtn);
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
-  deleteButton.classList.add('medium-buttons')
-  tasklist.appendChild(deleteButton);
+  deleteButton.classList.add("medium-buttons");
+  deleteButton.onclick = () => container.remove();
+  container.appendChild(deleteButton);
+  tasks.appendChild(container);
+}
+
+function save() {
+  const data = document.getElementById("tasks");
+  const nesteddivData = data.querySelector(".nested-div .target-paragraph");
+  const paragraph = nesteddivData.textContent;
+  localStorage.setItem("data", paragraph);
+}
+
+function onclear() {
+  alert("1");
+  localStorage.removeItem("data");
 }
