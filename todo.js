9f651d955c8b7c18ad2665a1ccf3312426c71905
HemasaiAmujuri <hemasaiamujuri3@gmail.com>
2025-07-21 22:41:27 -0700

    work in progress

--- todo.js
+++ todo.js
@@ -1,11 +1,15 @@
-function addTask(){
-    const tasklist = document.getElementById("task-list");
-    const value = document.getElementById("userInput").value;
-    let listItem = document.createElement('li');
-    listItem.innerText=value;
-    tasklist.appendChild(listItem);   
-}                                                                  
-
-
-
-
+function addTask() {
+  const tasklist = document.getElementById("task-list");
+  const value = document.getElementById("userInput").value;
+  let listItem = document.createElement("li");
+  listItem.innerText = value;
+  tasklist.appendChild(listItem);
+  const edit = document.createElement("button");
+  edit.textContent = "Edit"
+  edit.classList.add('medium-buttons')
+  tasklist.appendChild(edit);
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add('medium-buttons')
+  tasklist.appendChild(deleteButton);
+}
